9bbc176de9a02e13b220bacee719d274d787ecfe
Ronak Shah <ronakshahultimate@gmail.com>
2020-04-27 19:44:01 -0700

    Cleaned up Repo

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 
-/// rewrite this to be actually webscraping and whatnot
+/// Runs Python Webscraping Code
 async function randomQuote() {
     let getquote = new Promise((success, nosuccess) => {
 
         const { spawn } = require('child_process');
         const pyprog = spawn('python', ['./webscrape.py']);
-    
-        // console.log("running")
 
         pyprog.stdout.on('data', function(data) {
             success(data);
@@ -27,17 +25,18 @@ async function randomQuote() {
     }
 } 
 
-var Twit = require('twit'),
-config = {
-twitter: {
-consumer_key: process.env.CONSUMER_KEY,
-consumer_secret: process.env.CONSUMER_SECRET,
-access_token: process.env.ACCESS_TOKEN,
-access_token_secret: process.env.ACCESS_TOKEN_SECRET
-}
-},
-T = new Twit(config.twitter);
+const Twit = require('twit'),
+    config = {
+        twitter: {
+            consumer_key: process.env.CONSUMER_KEY,
+            consumer_secret: process.env.CONSUMER_SECRET,
+            access_token: process.env.ACCESS_TOKEN,
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        }
+    },
+    T = new Twit(config.twitter);
 
+/// main function that puts everything together
 async function main() {
     const quote = await randomQuote();
     console.log(`Attempting to Tweet ${quote}`)
@@ -51,7 +50,7 @@ async function main() {
             console.log("Great success!");
             console.log(response.url)
         }
-        });        
+    });        
 }
 
 main();
